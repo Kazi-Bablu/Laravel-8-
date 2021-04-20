c541c6513798b814150f18cc1ce5a638428099f7
kazi bablu <bablu@shuttlebd.com>
2021-04-21 01:35:30 +0600

    database connect

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
@@ -10,12 +11,17 @@ class UsersController extends Controller
 
     public function loadView()
     {
-        $data = ['1','2','3','4','5'];
-        return view('users',['users'=>$data]);
+        $data = ['1', '2', '3', '4', '5'];
+        return view('users', ['users' => $data]);
     }
 
     public function getData(Request $request)
     {
         dd($request->all());
+    }
+
+    public function index()
+    {
+        return DB::select('select * from users');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::view("about", "about");
 
 //Route::get("users",[UsersController::class,'loadView']);
 Route::post("users", [UsersController::class, 'getData']);
+Route::get("database", [UsersController::class, 'index']);
 Route::view("login", "users.users");
 
 Route::group(['middleware' => ['protectPage']],function(){
